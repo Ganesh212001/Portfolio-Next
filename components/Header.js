730ef0e437856277736472e35fcabe0b0a96f52e
Ganesh <ganeshmahajan21062001@gmail.com>
2023-09-21 18:27:48 +0530

    portfolio

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import Socials from '../components/Socials'
+
 const Header = () => {
-  return <header>Header</header>;
+  return(
+  <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+    <div className="container mx-auto">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+        <Link href={'/'}>
+            <h1 className="hover:text-accent"><span className="text-3xl font-bold text-gray-900 dark:text-white">G</span><span className="text-2xl font-normal text-gray-900 dark:text-white">anesh.</span></h1>
+        </Link>
+        {/* Socials */}
+        <Socials />
+      </div>
+    </div>
+  </header>
+  )
 };
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css';
+import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+import Router, { useRouter } from 'next/router';
+import { AnimatePresence,motion } from 'framer-motion';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>Portfolio | Ganesh Mahajan</title>
+        <meta name='discription' content='portfolio Ganesh Mahajan'/>
+      </Head>
+      <Layout>
+        <AnimatePresence mode='wait'>
+          <motion.div key={router.route} className='h-full'>
+            <Transition/>
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
+    )
 }
 
 export default MyApp;
