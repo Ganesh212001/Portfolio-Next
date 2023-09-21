5a73b6d0e70d4614fc75a56406fcd0524203ec11
Ganesh <ganeshmahajan21062001@gmail.com>
2023-09-21 18:41:43 +0530

    about change

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,11 +1,11 @@
 import {BsArrowRight} from 'react-icons/bs'
-import Circles from '../../components/Circles'
 import { motion } from 'framer-motion'
 import { fadeIn } from '../../variants'
 
 const Contact = () => {
   return (
     <div className='bg-primary/30'>
+      
       <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
         <div className='flex flex-col w-full max-w-[700px]'>
           {/* text */}
